43ec68e3a5c34f5f946120334bdc12ed258f19ad
Yuri1427934 <halo010354@gmail.com>
2023-02-13 12:56:21 -0700

    update head

--- Assets/TitleCharacter.cs
+++ Assets/TitleCharacter.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TitleCharacter : MonoBehaviour
 {
     public Rigidbody2D Head;
 
+    public HingeJoint2D HeadJoint;
+
     public Rigidbody2D Sholder_1;
 
     public Rigidbody2D Sholder_2;
+    private float HeadAngle;
+    [SerializeField]
+    private float HeadShakeCounter;
+    private void Start()
+    {
+        HeadShakeCounter = 0;
+        if (Head != null)
+            HeadAngle = Head.rotation;
+    }
 
     private void FixedUpdate()
     {
@@ -23,6 +35,24 @@ public class TitleCharacter : MonoBehaviour
             float AngleDeg = (180 / Mathf.PI) * AngleRad;
 
             Head.MoveRotation(AngleDeg);
+            if (Mathf.Abs(HeadAngle - AngleDeg) >= 30)
+            {
+                HeadAngle = AngleDeg;
+                HeadShakeCounter++;
+                if (HeadShakeCounter >= 20f && GameSettingScript.instance != null && GameSettingScript.instance.Explosion)
+                {
+                    Debug.Log("Oops");
+                    HeadJoint.enabled = false;
+                    Head.velocity = Vector3.zero;
+                    Head.AddForce(new Vector2(100f, 0f));
+                    Head.gravityScale = 1f;
+                    Head = null;
+                }
+                else if (HeadShakeCounter >= 20f)
+                {
+                    HeadShakeCounter = 0;
+                }
+            }
         }
     }
 
